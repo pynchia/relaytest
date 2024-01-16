cff561099c24c7056f2e17775a602701368ea36c
pynchia <pynchia@gmail.com>
2024-01-16 14:45:42 +0100

    structure cal earnings functionlity using composition

--- app/crud/earnings.py
+++ app/crud/earnings.py
@@ -1,55 +1,125 @@
 
 from sqlmodel import Session, select
 
-from app import schemas
+from app.schemas import Earnings, LineItem
 from app.models import Activity
 from app.tiers import Tiers
 
 
-class CRUDEarnings:
-    def __init__(self):
-        self.tiers_min_earnings = {
-            Tiers.bronze: 14.50,
-            Tiers.silver: 13.50,
-            Tiers.gold: 15.0,
-            Tiers.platinum: 15.25,
-        }
-
+class BronzeEarnings:
     BRONZE_PER_SUCCESSFUL_ATTEMPT = 0.459
     BRONZE_PER_UNSUCCESSFUL_ATTEMPT = 0.229
     BRONZE_LONG_ROUTE_BONUS = 10.0
     BRONZE_LOYALTY_BONUS = 20.0
 
+    def __init__(self):
+        self.hourly_min_earnings = 14.50
 
-    async def calc_earnings(self, db: Session, request_id: str, tier:Tiers) -> schemas.Earnings:
-        earnings = await getattr(self, 'calc_'+tier)(db, request_id)  # map by name convention
-        # now add the common attrs
-        earnings.hours_worked = 12
-        earnings.minimum_earnings = self.tiers_min_earnings[tier]
-        return earnings
-    
-    async def calc_bronze(self, db: Session, request_id: str):
+    async def calc_earnings(self, db: Session, request_id: str):
         activities = db.exec(
             select(Activity).where(Activity.request_id == request_id)
         )
-        return schemas.Earnings(
-
+        return Earnings(
+            line_items=[
+                LineItem(
+                    name=,
+                    quantity=,
+                    rate=self.BR,
+                    total=
+                )
+            ]
         )
 
-    async def calc_silver(self, db: Session, request_id: str):
-        return schemas.Earnings(
-            
+
+class SilverEarnings:
+    SILVER_PER_SUCCESSFUL_ATTEMPT = 0.459
+    SILVER_PER_UNSUCCESSFUL_ATTEMPT = 0.229
+    SILVER_LONG_ROUTE_BONUS = 10.0
+    SILVER_LOYALTY_BONUS = 20.0
+
+    def __init__(self):
+        self.hourly_min_earnings = 13.50
+
+    async def calc_earnings(self, db: Session, request_id: str):
+        activities = db.exec(
+            select(Activity).where(Activity.request_id == request_id)
+        )
+        return Earnings(
+            line_items=[
+                LineItem(
+                    name=,
+                    quantity=,
+                    rate=self.BR,
+                    total=
+                )
+            ]
         )
 
-    async def calc_gold(self, db: Session, request_id: str):
-        return schemas.Earnings(
-            
+
+class GoldEarnings:
+    SILVER_PER_SUCCESSFUL_ATTEMPT = 0.459
+    SILVER_PER_UNSUCCESSFUL_ATTEMPT = 0.229
+    SILVER_LONG_ROUTE_BONUS = 10.0
+    SILVER_LOYALTY_BONUS = 20.0
+
+    def __init__(self):
+        self.hourly_min_earnings = 15.0
+
+    async def calc_earnings(self, db: Session, request_id: str):
+        activities = db.exec(
+            select(Activity).where(Activity.request_id == request_id)
+        )
+        return Earnings(
+            line_items=[
+                LineItem(
+                    name=,
+                    quantity=,
+                    rate=self.BR,
+                    total=
+                )
+            ]
         )
 
-    async def calc_platinum(self, db: Session, request_id: str):
-        return schemas.Earnings(
-            
+
+class PlatinumEarnings:
+    SILVER_PER_SUCCESSFUL_ATTEMPT = 0.459
+    SILVER_PER_UNSUCCESSFUL_ATTEMPT = 0.229
+    SILVER_LONG_ROUTE_BONUS = 10.0
+    SILVER_LOYALTY_BONUS = 20.0
+
+    def __init__(self):
+        self.hourly_min_earnings = 15.25
+
+    async def calc_earnings(self, db: Session, request_id: str):
+        activities = db.exec(
+            select(Activity).where(Activity.request_id == request_id)
+        )
+        return Earnings(
+            line_items=[
+                LineItem(
+                    name=,
+                    quantity=,
+                    rate=self.BR,
+                    total=
+                )
+            ]
         )
 
+
+class CRUDEarnings:
+    def __init__(self):
+        self.bronze = BronzeEarnings()
+        self.silver = SilverEarnings()
+        self.gold = GoldEarnings()
+        self.platinum = PlatinumEarnings()
+
+    async def calc_earnings(self, db: Session, request_id: str, tier:Tiers) -> Earnings:
+        tier_instance = getattr(self, tier)
+        earnings = await tier_instance.calc_earnings(db, request_id)  # find the instance for the calculation by name convention
+        # now add the common attrs
+        earnings.hours_worked = 12
+        earnings.minimum_earnings = tier_instance.hourly_min_earnings*earnings.hours_worked
+        return earnings
+    
 
 earnings = CRUDEarnings()
--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -2,7 +2,7 @@ from typing import Any
 
 from pydantic import BaseModel
 
-from .earnings import Earnings
+from .earnings import Earnings, LineItem
 
 
 class Message(BaseModel):
